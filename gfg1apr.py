9ca6025c616b9de543eeaa93eaa99323e10eea5d
Akshat Gupta <akshatgupta@Akshats-MacBook-Air.local>
2026-04-01 20:10:35 +0530

    Complete gfg

---
+++ gfg1apr.py
@@ -0,0 +1,10 @@
+#consecutive 1's not allowed
+# Trick - Remember consecutive ones grow like fibonacci for small n's
+class Solution:
+    def countStrings(self, n):
+        if n<3:
+            return n+1 
+        a, b = 2, 3  
+        for _ in range(3, n + 1):
+            a, b = b, a + b
+        return b
